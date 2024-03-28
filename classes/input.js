25f4c37c8a030c2a51f57a1b4ab0d49558e17e9c
chuks <ikechukwuemeka60@gmail.com>
2024-03-28 16:52:49 +0100

    finito

--- classes/input.js
+++ classes/input.js
@@ -1,13 +1,26 @@
 export default class Input {
   constructor(game) {
-    game.canvas.addEventListener("mousemove", (e) => {
-      let canvasRectangle = game.canvas.getBoundingClientRect();
-      let touchX = e.clientX;
-      let touchY = e.clientY;
-      let paddle = game.components.paddle;
+    const canvas = game.canvas;
+
+    const handleMouseMove = (e) => {
+      const canvasRectangle = canvas.getBoundingClientRect();
+      let touchX, touchY;
+
+      if (e.type === "touchmove") {
+        touchX = e.touches[0].clientX;
+        touchY = e.touches[0].clientY;
+      } else {
+        touchX = e.clientX;
+        touchY = e.clientY;
+      }
+
+      const paddle = game.components.paddle;
 
       paddle.position.x = touchX - canvasRectangle.left - paddle.width / 2;
       paddle.position.y = touchY - canvasRectangle.top - paddle.height / 2;
-    });
+    };
+
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("touchmove", handleMouseMove);
   }
 }
